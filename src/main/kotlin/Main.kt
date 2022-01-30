9477e391e0848ddb16369f2d4451ba0b454342a2
salem404 <paururo08@hotmail.com>
2022-01-31 00:36:20 +0100

    Ejercicios completados

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,11 @@
-import kotlin.random.Random
 
 /* ---------------------------------
-* Fecha: 26/01/2022
-* Versión: 1.0
+* Fecha: 31/01/2022
+* Versión: 1.1
 * Autor: Paula Rumeu Romero
 * Empresa/centro: IES Rafael Alberti
 * Ejercicio: Kotlin inicial
 *----------------------------------- */
-
-
-fun main(args: Array<String>) {
-    val lista= IntArray(20)
-    
-}
 
 //Ejercicio 1
 fun notaImpresa(numero:Int) {
@@ -23,4 +16,42 @@ fun notaImpresa(numero:Int) {
         9,10 -> println("Sobresaliente")
         else -> println("Valor introducido no correcto")
     }
+}
+
+//Ejercicio 2
+fun main() {
+    notaImpresa(10)
+    println()
+
+
+    val lista = IntArray(20)
+
+    //Se llena y se muestran los números pares
+    for (numero in 0..19){
+        lista[numero] = (0..100).random()
+        if (lista[numero]%2==0){
+            println(lista[numero])
+        }
+    }
+
+    //Lista ordenada
+    println()
+    val ordenada = lista.sorted()
+
+    //Muestra el último y primer elemento
+    println(ordenada.last())
+    println(ordenada.first())
+
+    //Suma de elementos
+    println()
+    println(lista.sum())
+
+    //Añade los elementos pares a una nueva lista
+    val listaSinImpares = mutableListOf<Int>()
+    for (numero in lista){
+        if (numero %2==0){
+            listaSinImpares.add(numero)
+        }
+    }
+    println(listaSinImpares)
 }
